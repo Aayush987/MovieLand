d5dab1220a034ab9a4561e7848bfd5aa5e5a3db3
Aayush <aayushsharma47163@gmail.com>
2023-09-27 14:14:01 +0530

    Movieland Project Added

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState,useEffect } from 'react';
+import "./App.css";
+import SearchIcon from "/search.svg";
+import MovieCard from './MovieCard.jsx';
+
+const API_URL = "http://www.omdbapi.com?apikey=f0eed2b4";
+
+function App() {
+    const [Movies,setMovies] = useState([]);
+    const [searchTerm,setSearchTerm] = useState("");
+
+  const Searchmovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      
+      console.log(data.Search);
+      setMovies(data.Search);
+  }
+
+  useEffect(() => {
+        Searchmovies("Batman");
+  },[])
+  
+
+  return (
+    <div className="App">
+      <h1 className= "title">Movieland</h1>
+
+      <div className="search">
+        <input value = {searchTerm} onChange={(e) => {setSearchTerm(e.target.value)}} placeholder= "Search for movies" />
+        <img src = {SearchIcon} alt = "searchicon" onClick={() => {Searchmovies(searchTerm)}} />
+      </div>
+      
+   
+   {Movies?.length > 0 ?
+           (
+            <div className="container">
+              {Movies.map((movies) => (
+                  <MovieCard key={movies.imdbID} movie = {movies} />
+               ))} 
+               </div>
+           ):
+           (
+            <div className="empty">
+              <h2>No Movies Found</h2>
+            </div>
+           )
+   }
+    </div>
+  )
+}
+
+export default App
---
+++ src/MovieCard.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+const MovieCard = ({ movie: {Year, Poster, Title, Type } }) => {
+  return (
+        <div className="movie">
+            <div>
+               <p>{Year}</p>
+            </div>
+            <div>
+                <img src = {Poster != "N/A" ? Poster : 'https://via.placeholder.com/400'} alt = {Title} />
+            </div>
+            <div>
+            <span>{Type}</span>
+            <h3>{Title}</h3>
+            </div>
+        </div>
+  )
+}
+
+export default MovieCard;
